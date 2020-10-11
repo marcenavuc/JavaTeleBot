19e1f6cd13e15525a9d8a74a502ae8e1902aa583
mark <markenavuk@bk.ru>
2020-10-11 20:04:32 +0500

    After merge

--- JavaBot/src/main/java/Bot/Commands/Weather.java
+++ JavaBot/src/main/java/Bot/Commands/Weather.java
@@ -7,6 +7,7 @@ public class Weather extends Command {
 
     @Override
     public String execute(Message message, Repository manager) {
+
         return "Здесь будет функционал вывода погоды";
     }
 }
