dcc9864eb58a7c4095d15d3f1bf61ff15a7482b9
mark <markenavuk@bk.ru>
2020-11-10 13:02:11 +0500

    Small fixes

--- JavaBot/src/main/java/Bot/Models/WeatherApiJSON.java
+++ JavaBot/src/main/java/Bot/Models/WeatherApiJSON.java
@@ -1,11 +1,6 @@
 package Bot.Models;
 
-import Bot.Commands.Command;
 import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class WeatherApiJSON extends JSONObject {
     public String statusCode;
@@ -13,7 +8,7 @@ public class WeatherApiJSON extends JSONObject {
     public Double temperature;
     public String city;
     public Double feelsLike;
-    public String pressure;
+    public Integer pressure;
     public Double windSpeed;
     public String weatherDescription;
     public String icon;
@@ -26,7 +21,7 @@ public class WeatherApiJSON extends JSONObject {
             temperature = (Double) lastDay.getJSONObject("main").get("temp");
             city = jsonObject.getJSONObject("city").get("name").toString();
             feelsLike = (Double) lastDay.getJSONObject("main").get("feels_like");
-            pressure = lastDay.getJSONObject("main").get("pressure").toString();
+            pressure = (Integer) lastDay.getJSONObject("main").get("pressure");
             windSpeed = (Double) lastDay.getJSONObject("wind").get("speed");
             String iconName = lastDay.getJSONArray("weather")
                     .getJSONObject(0)
@@ -58,25 +53,4 @@ public class WeatherApiJSON extends JSONObject {
     public boolean isGoodCode() {
         return statusCode.equals("0");
     }
-
-   // private String iconMapper(String iconName) {
-
-//        Map<String, String> mapper = new HashMap<>();
-//            mapper.put("Clear", "☀");
-//            mapper.put("Rain", "☔");
-//            mapper.put("Snow", "❄");
-//            mapper.put("Clouds", "☁");
-//            String result = mapper.get(weatherDescription);
-//            return isNull(result) ? "" : result;
-
-//        ArrayList<String> mapper = new ArrayList<String>();
-//        mapper.add("\u2600\uFE0F");
-//        mapper.add("\uD83C\uDF24");
-//        mapper.add("\u2601\uFE0F");
-//        mapper.add("\uD83C\uDF2B");
-//        mapper.add("\uD83C\uDF27");
-//        mapper.add("\uD83C\uDF27");
-//        String result = mapper.get(Integer.parseInt(iconName.substring(0, 1)));
-//        return isNull(result) ? "" : result;
-  //  }
 }
